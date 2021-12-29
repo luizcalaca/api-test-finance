5e60082e9e03afeef5658b9376809a2ad6f4e1fb
Luiz Calaça <luizcalaca@Luizs-MacBook-Air.local>
2021-12-29 05:32:24 -0300

    add users test

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,16 +1,15 @@
 const request = require('supertest')
 
-const app = require('../src/app')
+const app = require('../../src/app')
 
 test('Deve listar todos os usuários', async () => {
     await request(app).get('/users').then( (res) => {
         expect(res.status).toBe(200)
-        expect(res.body).toHaveLength(1)
-        expect(res.body[0]).toHaveProperty('name', 'John')
+        expect(res.body).toHaveLength(0)
     })
 })
 
-test('Deve inserir o usuário com sucesso',  async () => {
+test.only('Deve inserir o usuário com sucesso',  async () => {
     await request(app).post('/users').send({name: 'Luiz', mail: 'luizcalaca@gmail.com'})
     .then((res) => {
         expect(res.status).toBe(201)
